b2e8ef925e8c1783c5739d8a5edb375d51880f73
vitaminvp <vitamin@ukr.net>
2019-07-13 13:51:59 +0300

    Updates

---
+++ precache-manifest.90a4458d5ba6a9cbaab7f0701fe9ac03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba719e59c7e31fb4b321acd94008efc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ee65849b932f39aecbb",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "08ff270bb0ed59e34714",
+    "url": "/static/js/2.d0ae188b.chunk.js"
+  },
+  {
+    "revision": "9ee65849b932f39aecbb",
+    "url": "/static/js/main.52662b0f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
